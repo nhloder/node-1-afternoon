3a2664240395cb6514e93e645da45be1d9da0b7d
nhloder <nhloder@gmail.com>
2019-10-17 16:04:09 -0600

    dun

--- server/index.js
+++ server/index.js
@@ -1,9 +1,14 @@
 const express = require('express');
- 
+const getProducts = require('./getProducts')
+const getProduct= require('./getProduct')
 const app = express();
- 
+
 const port = 4001
- 
+
+app.get('/api/products', getProducts)
+
 app.listen (port, () => {
-   console.log(`server listening on port ${port}`);
+    console.log(`server listening on port ${port}`);
 });
+
+app.get('/api/product/:id',getProduct)
